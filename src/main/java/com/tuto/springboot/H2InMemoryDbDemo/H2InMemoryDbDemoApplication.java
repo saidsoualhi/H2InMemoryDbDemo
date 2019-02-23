46a0bea32e93ed92de8f555ef3041a830612ca93
said soualhi <soualhi.said@gmail.com>
2019-02-23 12:13:21 +0100

    first commit

---
+++ src/main/java/com/tuto/springboot/H2InMemoryDbDemo/H2InMemoryDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tuto.springboot.H2InMemoryDbDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2InMemoryDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2InMemoryDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuto/springboot/H2InMemoryDbDemo/H2InMemoryDbDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tuto.springboot.H2InMemoryDbDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class H2InMemoryDbDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
